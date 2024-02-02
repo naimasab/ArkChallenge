cc375517d2d72ef88afa0d6b901710bb66c73d57
naima <naimasabri1504@gmail.com>
2024-02-02 09:04:27 +0100

    commit

--- src/arkXacademy/Day9/C4/QueueImplementation.java
+++ src/arkXacademy/Day9/C4/QueueImplementation.java
@@ -1,4 +1,21 @@
 package arkXacademy.Day9.C4;
-
+import java.util.*;
 public class QueueImplementation {
+    public static void main(String[] args) {
+        Queue<String> q = new LinkedList<String>();
+        q.add("Nora");
+        q.add("Nor");
+        q.add("Rim");
+        q.add("Ashraf");
+        q.add("Leila");
+        System.out.println("Removed element: "+q.remove());
+        System.out.println(("element at the front of the queue : " + q.peek()));
+        System.out.println("checking if the q is empty :" );
+
+        for( String str : q){
+            System.out.println(str);
+        }
+
+
+    }
 }
---
+++ src/arkXacademy/Day9/C5/LinkedListOperation.java
@@ -0,0 +1,16 @@
+package arkXacademy.Day9.C5;
+import java.util.*;
+public class LinkedListOperation {
+    public static void main(String[] args) {
+        LinkedList<String> Grocery =new LinkedList<>();
+        Grocery.add("Pasta");
+        Grocery.add("Orange");
+        Grocery.add("Tea");
+
+        Grocery.add(0, "Egg");
+        Grocery.remove(1);
+        int length = Grocery.size();
+        System.out.println("Length of the linked list: " + length);
+
+    }
+}
